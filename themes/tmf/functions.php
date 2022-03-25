bd3bb6798ef7c0fb2e22bc170213fb1f5be3b8ca
Dan Scott <danscott2150@gmail.com>
2022-03-25 16:31:28 -0500

    registering news article cpt

--- themes/tmf/functions.php
+++ themes/tmf/functions.php
@@ -3,3 +3,5 @@
 	require get_template_directory() . '/inc/post-types/class-news-article.php';
 	require get_template_directory() . '/inc/post-types/class-stock-recommendation.php';
 	require get_template_directory() . '/inc/taxonomies/class-stock-ticker.php';
+
+	new News_Article();
--- themes/tmf/inc/post-types/class-news-article.php
+++ themes/tmf/inc/post-types/class-news-article.php
@@ -8,4 +8,41 @@
 	 *  - Custom taxonomy: Stock Ticker
 	 */
 
-	class News_Article{}
+	class News_Article {
+
+		public function __construct(){
+			add_action( 'init', [ $this, 'register_cpt' ] );
+		}
+
+
+		public function register_cpt() {
+
+			$labels = array(
+				'name'          => __( 'News Articles' ),
+				'singular_name' => __( 'News Article' ),
+				'menu_name'     => __( 'News Articles' ),
+				'add_new'       => __( 'Add New Article' ),
+    			'add_new_item'  => __( 'Add New Article' ),
+				'not_found'     => __( 'No News Articles Found' ),
+			);
+
+			$args = array(
+				'label'         => __( 'News Article' ),
+				'labels'        => $labels,
+				'supports'      => [ 'title', 'editor', 'author', 'thumbnail' ],
+				'taxonomies'    => [ 'stock_ticker' ],
+				'public'        => true,
+				'menu_position' => 5,
+				'menu_icon'     => 'dashicons-media-document',
+				'has_archive'   => true,
+				'rewrite'       => [ 'slug' => 'news-article' ],
+
+			);
+
+			register_post_type( 'news_article', $args );
+
+		}
+
+
+
+	}
